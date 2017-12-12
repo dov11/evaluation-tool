afeb5f6d80ebe5263f66efcee82367471e8b8f26
dov11 <ovo.dan@gmail.com>
2017-12-12 22:53:57 +0100

    add router and signin-signout

--- src/components/ui/Navigation.js
+++ src/components/ui/Navigation.js
@@ -1,6 +1,8 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import { push } from 'react-router-redux'
+import signOut from '../../actions/user/sign-out'
 import AppBar from 'material-ui/AppBar'
 import IconButton from 'material-ui/IconButton'
 import AssesmentIcon from 'material-ui/svg-icons/action/assessment'
@@ -15,15 +17,15 @@ class Navigation extends PureComponent {
 
   signOut = (event) => {
     event.preventDefault()
-    // implement later
+    this.props.signOut()
   }
 
-  signUp = () => {
-    // implement later
+  signIn = () => {
+    this.props.push('/sign-in')
   }
 
   goHome = () => {
-    // implement later
+    this.props.push('/')
   }
 
   render() {
@@ -38,7 +40,7 @@ class Navigation extends PureComponent {
         }
         iconElementRight={signedIn ?
           <FlatButton label="Sign out" onClick={this.signOut.bind(this)} /> :
-          <FlatButton label="Sign up" onClick={this.signUp} />
+          <FlatButton label="Sign in" onClick={this.signIn} />
         }
       />
     )
@@ -49,4 +51,4 @@ const mapStateToProps = ({ currentUser }) => ({
   signedIn: (!!currentUser && !!currentUser._id)
 })
 
-export default connect(mapStateToProps)(Navigation)
+export default connect(mapStateToProps, { push, signOut })(Navigation)
---
+++ src/routes.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
+
+import {
+  // Lobby,
+  SignIn,
+  // SignUp
+} from './containers'
+
+export default class Routes extends Component {
+  render() {
+    return (
+      <div>
+        <Route path="/sign-in" component={SignIn} />
+      </div>
+    )
+  }
+}
+
+// <Route path="/sign-up" component={SignUp} />
+// <Route exact path="/" component={Lobby} />
